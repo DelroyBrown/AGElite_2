fb31f2a57189fd39643ce5b4bd1bdec077cf7969
Delroy Brown <delroybrown.db@gmail.com>
2025-05-15 19:43:49 +0100

    first commit

---
+++ AGElite_store/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Product, Category, Order, OrderItem
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name")}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "available"]
+    list_filter = ["available", "category"]
+    prepopulated_fields = {"slug": ("name")}
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "paid", "created"]
+    inlines = [OrderItemInline]
---
+++ AGElite_store/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AgeliteStoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AGElite_store'
