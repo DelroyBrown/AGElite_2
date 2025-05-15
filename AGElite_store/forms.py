168ba8cde70f3288b352d3b1d804d9708f5ab7b0
Delroy Brown <delroybrown.db@gmail.com>
2025-05-15 20:20:29 +0100

    Added product listing and detail views with category filtering and cart functionality

---
+++ AGElite_store/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Order
+
+
+class CartAddProductForm(forms.Form):
+    quantity = forms.IntegerField(min_value=1, initial=1)
+    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+
+
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fieldds = ["first_name", "last_name", "email", "address", "postal_code", "city"]
--- AGElite_store/views.py
+++ AGElite_store/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Category, Product, Order, OrderItem
+from .forms import CartAddProductForm, OrderCreateForm
+from .cart import Cart
 
-# Create your views here.
+
+# Product listing by category
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(
+        request,
+        "store/product_list.html",
+        {"category": category, "categories": categories, "products": products},
+    )
+
+
+# Product detail
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
+    return render(
+        request,
+        "store/product_detail.html",
+        {"product": product, "cart_product_form": cart_product_form},
+    )
+
+
+# Add/Remove from cart
